5ced32c8ae7b0991cdb72be740d0ddef0ed51181
Charly-Less <charlylessard@gmail.com>
2025-02-20 15:53:28 -0500

    added for loop to change parameters

--- finding_steady_state.jl
+++ finding_steady_state.jl
@@ -1,24 +1,27 @@
-using Aggregations
+#using Aggregations
 using Distances
 using DifferentialEquations
 using NonlinearSolve, SteadyStateDiffEq, OrdinaryDiffEq
 using LinearAlgebra
 using Plots
+<<<<<<< HEAD
+=======
 
+>>>>>>> fb6e392c1e971328317c189617dbb725d59884d6
 
 include("functions.jl")
 
 ### Parameters 
-N = 200
+N = 50
 
 ### Kernel Parameters
-λ = 0.2
-α = 25.0
+λ = 0.01
+α = 25
 
 p = (α, λ, N)
-
+ 
 ### Initial condition
-u = vec(2 * rand(3, N) .- 1)
+u0 = vec(2 * rand(3, N) .- 1)
 
 ### How bad is the initial condition?
 # norm(u, Inf)
@@ -27,7 +30,7 @@ ff_hessian = ODEFunction(g!; jac=h!)
 ff = ODEFunction(g!)
 
 
-type = eltype(u[1])
+type = eltype(u0[1])
 r = zeros(type, N, N)
 a = zeros(type, N, N)
 f = zeros(type, N, N)
@@ -42,13 +45,49 @@ Ya = zeros(type, N, N)
 p = (α, λ, N, r, f, a, XX, YY, ZZ, YX, ZX, ZY, Xa, Ya)
 
 ### Solving
-@time u = solve(SteadyStateProblem(ff_hessian, u[:], p), DynamicSS(TRBDF2()))
+@time u = solve(SteadyStateProblem(ff_hessian, u0[:], p), DynamicSS(TRBDF2()))
 
 #### How close are we to the steady state?
-norm(g(u,p), Inf)
-
+print(norm(g(u,p), Inf))
 
 ### Plots
+<<<<<<< HEAD
+#u = reshape(u,  N,3)
+#scatter(u[:,1], u[:,2], u[:,3], m=(3, 0.8, :blues, stroke(0)))
+
+
+# Define a range of alpha values to test
+alpha_values = 30:5:100
+lambda_values = [0.01]
+
+for λ in lambda_values
+    for α in alpha_values
+        println("Solving for α = $α, λ = $λ")
+    
+    # Update parameters
+        p = (α, λ, N, r, f, a, XX, YY, ZZ, YX, ZX, ZY, Xa, Ya)
+    
+    # Solve the system
+        @time u = solve(SteadyStateProblem(ff_hessian, u[:], p), DynamicSS(TRBDF2()))
+
+    # Compute how close we are to the steady state
+        println("Norm: ", norm(g(u, p), Inf))
+    
+    #plot solutions
+        u = reshape(u,  N,3)
+        plot = scatter(u[:,1], u[:,2], u[:,3], m=(3, 0.8, :blues, stroke(0)), title = "α = $α, λ=$λ")
+        display(plot)
+    end
+end
+
+
+
+
+
+
+
+=======
 u = reshape(u,  N,3)
 scatter(u[:,1], u[:,2], u[:,3], m=(3, 0.8, :blues, stroke(0)))
+>>>>>>> fb6e392c1e971328317c189617dbb725d59884d6
 
